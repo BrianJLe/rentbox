5d7c60f5cc91f24d4a1352eafac7d1f95628de61
brianlethal <litoblee@gmail.com>
2015-02-03 10:20:53 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rentbox::Application.config.secret_token = 'dd607097e93ae3b3e2a3b1291cbcfb1674d93f4dee629e4eecdb13be92b588329657ac9cd04459ab17e45e02e71935cf61013c8d00efb7271af7e9f7fba73414'
